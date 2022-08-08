f304cab034f4a766e835b65357c85c95a1733a0d
Prashant Singh <octanesingh@gmail.com>
2022-08-09 04:25:23 +0530

    Right infinite scroll working

--- src/components/animatedCarousel.jsx
+++ src/components/animatedCarousel.jsx
@@ -3,43 +3,6 @@ import { css } from "@emotion/css";
 import Slide from "./slide";
 
 export default function AnimatedCarousel() {
-  let height = 100;
-  let slideWidth = 280 + 20;
-  let transitionTime = 500;
-  let infiniteScroll = true;
-  let autoplay = false;
-  let step = 2;
-  let carouselWidth = 900;
-  let amount = step * slideWidth;
-
-  const [isScrolling, setIsScrolling] = useState(false);
-  const [isLeftScroll, setIsLeftScroll] = useState(false);
-  const [isRightScroll, setIsRightScroll] = useState(false);
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [state, setState] = useState([5, 6, 0, 1, 2, 3, 4]);
-
-  const scrollLeft = () => {
-    setIsScrolling(true);
-    setIsLeftScroll(true);
-    setTimeout(() => {
-      setIsLeftScroll(false);
-      setIsScrolling(false);
-      //update the state magic
-      setState([3, 4, 5, 6, 0, 1, 2]);
-    }, transitionTime);
-  };
-  const scrollRight = () => {
-    setIsScrolling(true);
-    setIsRightScroll(true);
-
-    setTimeout(() => {
-      setIsRightScroll(false);
-      setIsScrolling(false);
-      //update the state magic
-      setState([0, 1, 2, 3, 4, 5, 6]);
-    }, transitionTime);
-  };
-
   let data = [
     "Slide 1",
     "Slide 2",
@@ -50,6 +13,91 @@ export default function AnimatedCarousel() {
     "Slide 7",
   ];
 
+  let height = 100;
+  let slideWidth = 280 + 20;
+  let transitionTime = 500;
+  let infiniteScroll = true;
+  let autoplay = false;
+  let step = 2;
+  let carouselWidth = 900;
+  let numberOfSlides = Math.floor(carouselWidth / slideWidth);
+  let amount = step * slideWidth;
+  let totalItems = numberOfSlides + 2 * step;
+
+  const [isScrolling, setIsScrolling] = useState(false);
+  const [isLeftScroll, setIsLeftScroll] = useState(false);
+  const [isRightScroll, setIsRightScroll] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [state, setState] = useState([]);
+
+  useEffect(()=> {
+    setState([...generateState(activeIndex,step,data.length,numberOfSlides)]);
+  },[activeIndex])
+
+  const generateState =(currentIndex,step,length,numberOfSlides) =>{
+    let arr = [];
+      if(length===0){
+        return [];
+      }
+  
+      //calculate the first hidden ones
+      for(let i=1;i<=step;i++)
+        {
+          arr.unshift((currentIndex-i)%length);
+        }
+  
+      for(let i=0;i<arr.length;i++){
+        if(arr[i]<0){
+          arr[i] = arr[i] + length;
+        }
+      }
+  
+      let arr2=[];
+      //central vista
+      for(let i=0;i<numberOfSlides;i++){
+        arr2.push((i+currentIndex)%length);
+      }
+  
+      let base = arr2[arr2.length-1];
+      let arr3 = [];
+      //ending
+      for(let i=1;i<=step;i++){
+        arr3.push((i+base)%length);
+      }
+      console.log(arr);
+      console.log(arr2);
+      console.log(arr3);
+  
+      return[...arr,...arr2,...arr3];
+  
+    }
+  
+
+  
+
+  const scrollLeft = () => {
+    setIsScrolling(true);
+    setIsLeftScroll(true);
+    setTimeout(() => {
+      setIsLeftScroll(false);
+      setIsScrolling(false);
+      //update the index
+      setActiveIndex(activeIndex);
+    }, transitionTime);
+  };
+  const scrollRight = () => {
+    setIsScrolling(true);
+    setIsRightScroll(true);
+
+    setTimeout(() => {
+      setIsRightScroll(false);
+      setIsScrolling(false);
+      //update the state magic
+      setActiveIndex((activeIndex + step) % data.length);
+    }, transitionTime);
+  };
+
+  
   return (
     <div
       id="carousel"
@@ -73,8 +121,8 @@ export default function AnimatedCarousel() {
           ${isScrolling && isRightScroll && `margin-left:${-2 * amount}px;`}
         `}
       >
-        {state.map((index) => (
-          <Slide key={index} data={index}></Slide>
+        {state.map((index,seq) => (
+          <Slide key={seq} data={data[index]}></Slide>
         ))}
       </div>
       <div
